0fcdb2a74d6e1923bd8ecd325df5ff45bceae930
harsgak <harsgak@yahoo.com>
2018-09-29 08:17:22 +0530

    Set last data day as reset point
    Closes #6
    `new Date()` ie `today` is still kept as a fallback if anything else fails.

--- js/chrono-doze.js
+++ js/chrono-doze.js
@@ -315,7 +315,7 @@ d3.select("#nextWeek").on("click",function(){
 
 d3.select("#scrollButton").on("click", function(){
   console.log("scrollButton")
-  currentDay=new Date()
+  currentDay=lastdataDay || new Date()
   daysDisplay.remove()
   daysDisplay = createNCircle(chronos,inR,outR,gapRatio,getNdaysDataUpto(currentDay, numDays),numDays)
 })
@@ -409,10 +409,12 @@ d3.text(csvFile, function(error, text){
     console.log(allnapsdata)
     alldaysdata=groupByDate(allnapsdata)
     alldaysdata=colorMyDays(alldaysdata)
-    currentDay = new Date()
+    //max key code derived from https://stackoverflow.com/a/27376421
+    lastdataDay = Object.keys(alldaysdata)
+                    .reduce(function(a, b){ return alldaysdata[a].date > alldaysdata[b].date ? a : b });
+    currentDay = lastdataDay || new Date()
     createCircle(chronos,inR,outR,daydataticks)
     numDays = 14
-    //daysDisplay = create7Circle(chronos,inR,outR,gapRatio, weekdata)
     daysDisplay = createNCircle(chronos,inR,outR,gapRatio,getNdaysDataUpto(currentDay, numDays),numDays)
 
   });
